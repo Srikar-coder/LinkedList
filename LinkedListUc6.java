8861a4cd927bc3f8da2379e31a428315e5ee4ae6
Srikar <srikarsamanchi1999@gmail.com>
2021-06-09 22:09:40 +0530

    Added: deleting last node

---
+++ LinkedListUc6.java
@@ -0,0 +1,58 @@
+public class LinkedListUc6<T> {
+
+	private LinkedListNode<T> first = null;
+	//inserting into linkedlist
+        public void insertNode(LinkedListNode<T> node) {
+		node.setNext(first);
+		first = node;
+	}
+
+	public void removeNode(){
+		if(first.getNext()!=null)
+			first = first.getNext();
+		else first = null;
+	}
+	
+	private void printList(LinkedListNode<T> node) {
+		System.out.print(node.getValue()+"->");
+		if(node.getNext()!=null) printList(node.getNext());
+	}
+
+	public void print(){
+		printList(first);
+	}
+	
+	
+	public static void main(String[] args) {
+		LinkedListUc6<Integer> list = new LinkedListUc6<>();
+		list.insertNode(new LinkedListNode<Integer>(56));
+		list.insertNode(new LinkedListNode<Integer>(30));
+		list.insertNode(new LinkedListNode<Integer>(70));
+		list.print();
+		list.removeNode();
+		System.out.println("After removing the last node linked list is");
+		list.print();
+	}
+
+}
+
+class LinkedListNode<T> {
+	private T value;
+	private LinkedListNode<T> next;
+
+	public LinkedListNode(T value) {
+		this.value = value;
+	}
+
+	public void setNext(LinkedListNode<T> next) {
+		this.next = next;
+	}
+
+	public LinkedListNode<T> getNext() {
+		return next;
+	}
+
+	public T getValue() {
+		return value;
+	}
+}
